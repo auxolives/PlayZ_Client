ed472a3f74ac437f7fdcf597e0ba4fb4221989d4
Olivier <olivier.lecoq@outlook.com>
2026-06-03 19:57:29 +0200

    PlayZLighting: fix custom preset paths so lighting loads from the PBO.
    Use the PlayZ_Client PBO mount prefix and fall back to vanilla IDs via super.

--- PlayZLighting/scripts/3_game/WorldLighting.c
+++ PlayZLighting/scripts/3_game/WorldLighting.c
@@ -1,15 +1,13 @@
 modded class WorldLighting
 {
-	protected string lighting_PlayZ_default = "PlayZLighting\\DZ\\data\\lighting\\lighting_PlayZ_default.txt";
-	protected string lighting_PlayZ_darknight = "PlayZLighting\\DZ\\data\\lighting\\lighting_PlayZ_darknight.txt";
-	protected string lighting_DayZ063 = "PlayZLighting\\DZ\\data\\lighting\\lighting_PlayZ_DayZ063.txt";
-	protected string lighting_PlayZ_rework = "PlayZLighting\\DZ\\data\\lighting\\lighting_PlayZ_rework.txt";
-	protected string lighting_PlayZ_dark_rework = "PlayZLighting\\DZ\\data\\lighting\\lighting_PlayZ_dark_rework.txt";
+	protected string lighting_PlayZ_default = "PlayZ_Client\\PlayZLighting\\DZ\\data\\lighting\\lighting_PlayZ_default.txt";
+	protected string lighting_PlayZ_darknight = "PlayZ_Client\\PlayZLighting\\DZ\\data\\lighting\\lighting_PlayZ_darknight.txt";
+	protected string lighting_DayZ063 = "PlayZ_Client\\PlayZLighting\\DZ\\data\\lighting\\lighting_PlayZ_DayZ063.txt";
+	protected string lighting_PlayZ_rework = "PlayZ_Client\\PlayZLighting\\DZ\\data\\lighting\\lighting_PlayZ_rework.txt";
+	protected string lighting_PlayZ_dark_rework = "PlayZ_Client\\PlayZLighting\\DZ\\data\\lighting\\lighting_PlayZ_dark_rework.txt";
 	
 	override void SetGlobalLighting( int lightingID )
 	{
-		super.SetGlobalLighting( lightingID );
-
 		switch ( lightingID )
 		{
 			case 60:
@@ -30,6 +28,10 @@ modded class WorldLighting
 
 			case 64:
 				GetGame().GetWorld().LoadNewLightingCfg( lighting_PlayZ_dark_rework ); 
+				break;
+
+			default:
+				super.SetGlobalLighting( lightingID );
 				break;
 		}
 	}
