991fd79375bb93a1773a103fdc8a771f78ab99f3
Olivier <olivier.lecoq@outlook.com>
2026-06-03 20:19:52 +0200

    PlayZLighting: drop rework presets and IDs 63–64.
    Keep default, darknight, and DayZ063 profiles only.

--- PlayZLighting/scripts/3_game/WorldLighting.c
+++ PlayZLighting/scripts/3_game/WorldLighting.c
@@ -3,8 +3,6 @@ modded class WorldLighting
 	protected string lighting_PlayZ_default = "PlayZ_Client\\PlayZLighting\\DZ\\data\\lighting\\lighting_PlayZ_default.txt";
 	protected string lighting_PlayZ_darknight = "PlayZ_Client\\PlayZLighting\\DZ\\data\\lighting\\lighting_PlayZ_darknight.txt";
 	protected string lighting_DayZ063 = "PlayZ_Client\\PlayZLighting\\DZ\\data\\lighting\\lighting_PlayZ_DayZ063.txt";
-	protected string lighting_PlayZ_rework = "PlayZ_Client\\PlayZLighting\\DZ\\data\\lighting\\lighting_PlayZ_rework.txt";
-	protected string lighting_PlayZ_dark_rework = "PlayZ_Client\\PlayZLighting\\DZ\\data\\lighting\\lighting_PlayZ_dark_rework.txt";
 	
 	override void SetGlobalLighting( int lightingID )
 	{
@@ -20,14 +18,6 @@ modded class WorldLighting
 				
 			case 62:
 				GetGame().GetWorld().LoadNewLightingCfg( lighting_DayZ063 ); 
-				break;
-
-			case 63:
-				GetGame().GetWorld().LoadNewLightingCfg( lighting_PlayZ_rework ); 
-				break;
-
-			case 64:
-				GetGame().GetWorld().LoadNewLightingCfg( lighting_PlayZ_dark_rework ); 
 				break;
 
 			default:
